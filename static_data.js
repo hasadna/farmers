fd68a6743d268e17fc273256f9946fdf81c063ec
amitl <amitl@amitl-lpt.roam.corp.google.com>
2017-06-06 14:32:09 +0300

    page html - initial commit

---
+++ static_data.js
@@ -0,0 +1,26 @@
+/**
+ * Contains static data for the page's form controls.
+ */
+ 
+var data = {};
+
+data.dropdowns = {};
+
+// Maps from crop type to list of subtypes.
+data.dropdowns.crops = {  // Mock until we have real values.
+	"Avocado": [
+		"Round Avocado",
+		"Square Avocado"
+	],
+	"Banana": [
+		"Yellow Banana",
+		"Pink Banana"
+	]
+};
+
+data.dropdowns.locations = [  // Mock until we have real values.
+	"Haifa",
+	"Jerusalem",
+	"Tel Aviv"
+];
+
