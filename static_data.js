1b590a19c4c0294659707782c96c886dbda6c2e8
amitl <amitl@amitl-lpt.roam.corp.google.com>
2017-06-06 15:37:37 +0300

    page html - add crop subtype and separate location dropdown

--- static_data.js
+++ static_data.js
@@ -1,6 +1,7 @@
 /**
  * Contains static data for the page's form controls.
  */
+ // Using /*a*/ to prevent editors from RTL-ing source in Hebrew.
  
 var data = {};
 
@@ -8,19 +9,21 @@ data.dropdowns = {};
 
 // Maps from crop type to list of subtypes.
 data.dropdowns.crops = {  // Mock until we have real values.
-	"Avocado": [
-		"Round Avocado",
-		"Square Avocado"
-	],
-	"Banana": [
-		"Yellow Banana",
-		"Pink Banana"
-	]
+/*a*/	"אבוקדו": [
+/*a*/		"גליל מערבי",
+/*a*/		"חוף כרמל",
+/*a*/		"עמקים"
+/*a*/	],
 };
 
-data.dropdowns.locations = [  // Mock until we have real values.
-	"Haifa",
-	"Jerusalem",
-	"Tel Aviv"
-];
+data.dropdowns.stations = {  // Mock until we have real values.
+/*a*/	"תל אביב": [
+/*a*/		"תל אביב 1",
+/*a*/		"תל אביב 2",
+/*a*/	],
+/*a*/	"ירושלים": [
+/*a*/		"ירושלים 1",
+/*a*/		"ירושלים 2"
+/*a*/	]
+};
 
